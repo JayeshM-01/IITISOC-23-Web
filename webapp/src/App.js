941c817a02aca4837418d2f04873f129ea75c883
maripaba2 <jungharerishi@gmail.com>
2023-06-13 11:57:52 +0530

    added buttons

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
 import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+
+        <div className="nav">
         <Navbar />
+        </div>
+        
         <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
         </Routes>
       </Router>
--- webapp/src/Navbar.js
+++ webapp/src/Navbar.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/menu"> Menu </Link>
-      <Link to="/contact"> Contact </Link>
+    <div className="navi" >
+      <Link to="/"> <button> Home </button></Link>
+      <Link to="/menu"><button> Menu </button></Link>
+      <Link to="/contact"><button> Contact </button></Link>
+      <Link to="/login"><button> Login </button></Link>
     </div>
   );
 };
