faaafae8469ddf40b0919fd3e5dbf5f34d9a6abc
maripaba2 <jungharerishi@gmail.com>
2023-06-13 19:56:43 +0530

    stage

--- webapp/src/Navbar.js
+++ webapp/src/Navbar.js
@@ -6,9 +6,9 @@ export const Navbar = () => {
       <Link to="/menu"><button> Menu </button></Link>
       <Link to="/contact"><button> Contact </button></Link>
       <Link to="/login"><button> Login </button></Link>
-      <span className="logo">
-      <img align="right" src="https://th.bing.com/th/id/OIP.P3RCsEutAjthIKfbfLJ4dAHaGK?w=228&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="100hv" ></img>
-      </span>
+      {/* <span className="logo">
+      <img align="right" src="https://th.bing.com/th/id/OIP.P3RCsEutAjthIKfbfLJ4dAHaGK?w=228&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="72vh" max-width="100%" ></img>
+      </span> */}
       
     </div>
   );
--- webapp/src/pages/Login.js
+++
@@ -1,3 +0,0 @@
-export const Login = () => {
-    return <h1> THIS IS THE Login PAGE</h1>;
-  };
