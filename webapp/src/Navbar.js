6a422685c32f13d265fc950482dd3ea84306014b
maripaba2 <jungharerishi@gmail.com>
2023-06-13 12:43:01 +0530

    added logo

--- webapp/src/Navbar.js
+++ webapp/src/Navbar.js
@@ -6,6 +6,10 @@ export const Navbar = () => {
       <Link to="/menu"><button> Menu </button></Link>
       <Link to="/contact"><button> Contact </button></Link>
       <Link to="/login"><button> Login </button></Link>
+      <span className="logo">
+      <img align="right" src="https://th.bing.com/th/id/OIP.P3RCsEutAjthIKfbfLJ4dAHaGK?w=228&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="100hv" ></img>
+      </span>
+      
     </div>
   );
 };
