2665d56a2d4d1ac053f0d67f02ee8391ca6f7a14
maripaba2 <jungharerishi@gmail.com>
2023-06-19 19:19:09 +0530

    added counsellors

--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -1,3 +1,25 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Product from "../Product.js";
+import { productData, responsive } from "../data";
+
 export const Home = () => {
-    return <h1> THIS IS THE HOME PAGE</h1>;
+  
+  const product = productData.map((item) => (
+    <Product
+      name={item.name}
+      url={item.imageurl}
+      price={item.price}
+      description={item.description}
+    />
+  ));
+
+  return (
+    <div className="App">
+      <h1 className='heading'>Counsellers</h1>
+      <Carousel draggable={true}  swipeable={true} responsive={responsive}>
+        {product}
+      </Carousel>
+    </div>
+  );
   };
