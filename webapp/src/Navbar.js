67140a568ac02f818684edbf7717767b1ef485ef
maripaba2 <jungharerishi@gmail.com>
2023-06-21 19:07:30 +0530

    made navbar responsive

--- webapp/src/Navbar.js
+++ webapp/src/Navbar.js
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export const Navbar = () => {
+  const [extendNav,setExtendNav] = useState(false);
+  const showNavbar = () => {
+    setExtendNav(!extendNav);
+  }
+
   return (
+    <div>
     <div className="navi" >
+      <div className="navLinks">
       <Link to="/"> <button className="active"> Home </button></Link>
       <Link to="/menu"><button> Menu </button></Link>
       <Link to="/contact"><button> Contact </button></Link>
       <Link to="/login"><button> Login </button></Link>
-      {/* <span className="logo">
-      <img align="right" src="https://th.bing.com/th/id/OIP.P3RCsEutAjthIKfbfLJ4dAHaGK?w=228&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="72vh" max-width="100%" ></img>
-      </span> */}
-      
+      </div><div className="show"><button className="ham" onClick={() => showNavbar()}>{ extendNav ? <>&#10005;</> : <> &#8801;</>}</button></div>
     </div>
+    {extendNav && (<div id="shown" className= {extendNav ? "extendedNavbar" : ""}>
+    <div><Link to="/"> <button className="active"> Home </button></Link></div>
+    <div><Link to="/menu"><button> Menu </button></Link></div>
+    <div><Link to="/contact"><button> Contact </button></Link></div>
+    <div><Link to="/login"><button> Login </button></Link></div>
+  </div>)}
+  </div>
   );
 };
