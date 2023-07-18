e31a4b380ed77de0f3c2ec9b95dfc5ce2af98271
maripaba2 <jungharerishi@gmail.com>
2023-07-18 19:36:59 +0530

    made role base page

--- webapp/src/pages/Contact.js
+++ webapp/src/pages/Contact.js
@@ -3,13 +3,15 @@ import Axios from "axios";
 import "../pages/Contact.css";
 import { Navbar } from "../Navbar";
 import {Footer}  from "../components/Footer"
+import { Usercontact } from "./Usercontact";
+import { Admincontact } from "./Admincontact";
 
 export const Contact = () => {
   const [name, setName] = useState("");
   const [Email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
-
   const [msgList, setmsgList] = useState([]);
+  const [role,setRole] = useState("");
 
   const addqueries = () => {
     Axios.post("http://localhost:3001/create", {
@@ -34,52 +36,20 @@ export const Contact = () => {
     });
   },[]);
 
+  useEffect(() => {
+    Axios.get("http://localhost:3001/login").then((response) => {
+      if (response.data.loggedIn === true) {
+        setRole(response.data.user[0].role);
+      } else {
+        setRole("");
+      }
+    });
+  },[]);
+
   return (
     <div>
-
-    <div className="nav">
-       < Navbar/> 
-     </div>
-  
-    <div className="contact-us">
-      <div className="c-information">
-        <h2 className="c-heading">Contact Us</h2>
-        <div className="c-main-lable">
-        <label className="c-lable">Full Name
-        <input
-          className="c-input"
-          placeholder="eg. raja"
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-          />
-        </label>
-        <label className="c-lable">Email
-        <input
-          className="c-input"
-          placeholder="eg. help@gmail.com"
-          type="Email"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-          />
-        </label>
-          </div>
-        <label className="c-lable-msg">Explain Queries
-        <textarea
-          className="c-input-msg"
-          placeholder="please clear your dout"
-          type="text"
-          onChange={(event) => {
-            setMsg(event.target.value);
-          }}/>
-        </label>
-        <button onClick={addqueries} className="c-submit">Send</button>
-      </div>  
-    </div>
-  <Footer/>
-
+      {role === null && <Usercontact/>}
+      {role === "admin" && <Admincontact/>}
     </div>
   );
   };
---
+++ webapp/src/pages/Usercontact.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import "../pages/Contact.css";
+import { Navbar } from "../Navbar";
+import {Footer}  from "../components/Footer"
+
+export const Usercontact = () => {
+    const [name, setName] = useState("");
+    const [Email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
+    const [msgList, setmsgList] = useState([]);
+  
+    const addqueries = () => {
+      Axios.post("http://localhost:3001/create", {
+        name: name,
+        Email: Email,
+        msg: msg,
+      }).then(() => {
+        setmsgList([
+          ...msgList,
+          {
+            name: name,
+            Email: Email,
+            msg: msg,
+          },
+        ]);
+      });
+    };
+  
+    useEffect( () => {
+      Axios.get("http://localhost:3001/msg").then((response) => {
+        setmsgList(response.data);
+      });
+    },[]);
+  
+  
+    return (
+      <div>
+  
+      <div className="nav">
+         < Navbar/> 
+       </div>
+  
+    
+      <div className="contact-us">
+        <div className="c-information">
+          <h2 className="c-heading">Contact Us</h2>
+          <div className="c-main-lable">
+          <label className="c-lable">Full Name
+          <input
+            className="c-input"
+            placeholder="eg. raja"
+            type="text"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            />
+          </label>
+          <label className="c-lable">Email
+          <input
+            className="c-input"
+            placeholder="eg. help@gmail.com"
+            type="Email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            />
+          </label>
+            </div>
+          <label className="c-lable-msg">Explain Queries
+          <textarea
+            className="c-input-msg"
+            placeholder="please clear your dout"
+            type="text"
+            onChange={(event) => {
+              setMsg(event.target.value);
+            }}/>
+          </label>
+          <button onClick={addqueries} className="c-submit">Send</button>
+        </div>  
+      </div>
+    <Footer/>
+  
+      </div>
+    );
+    };
