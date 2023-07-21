3ae5445937b490647c2b1b64c83b83f94ee3d3b2
maripaba2 <jungharerishi@gmail.com>
2023-07-21 07:53:34 +0530

    change the css of contact and menu

--- webapp/src/pages/Admincontact.js
+++ webapp/src/pages/Admincontact.js
@@ -1,58 +1,110 @@
 import { Navbar } from "../Navbar";
-import {Footer}  from "../components/Footer"
+import { Footer } from "../components/Footer";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import "./Admincontact.css"
+import "./menu.css";
 
-export const Admincontact = () =>{
+export const Admincontact = () => {
+  const [msgList, setmsgList] = useState([]);
 
-    const [msgList, setmsgList] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/msg").then((response) => {
+      setmsgList(response.data);
+    });
+  }, []);
 
-    useEffect( () => {
-      Axios.get("http://localhost:3001/msg").then((response) => {
-        setmsgList(response.data);
-      });
-    },[]);
-  
-    const deleteMsg = (id) => {
-      Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
-        setmsgList(
-          msgList.filter((val) => {
-            return val.id != id;
-          })
-        );
-      });
-    };
+  const deleteMsg = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setmsgList(
+        msgList.filter((val) => {
+          return val.id != id;
+        })
+      );
+    });
+  };
 
-    return (
-        <div>
-        <div className="nav">
-         < Navbar/> 
-       </div>
-       <div className="consellers">
-      <h2 className="h-mainheading">HISTORY</h2>
-      <div className="c-querie">
-        {msgList.map((val, key) => {
-          return (
-            <div className="c-queries">
-              <div>
-              <li className="c-queries-li">Name :<h3 className="c-queries-ipt"> {val.name}</h3></li> 
-              <li className="c-queries-li">Email :<h3 className="c-queries-ipt"> {val.Email}</h3></li> 
-              <li className="c-queries-li">Message :<h3 className="c-queries-ipt"> {val.msg}</h3></li> 
-              </div>
-              <button className="c-d-btn" onClick={() => {deleteMsg(val.id);}}>
-                  Delete
-              </button>
-            </div>
-          );
-        })}
+  return (
+    <div>
+      <div className="nav">
+        <Navbar />
       </div>
-    </div>
+      <div className="consellers">
+        <h2 className="h-mainheading">Queries</h2>
+        <div className="c-querie">
+          <div className="mx-auto pb-8 w-full max-w-7xl overflow-x-auto">
+            <table className="px-4 min-w-full rounded-md border border-gray-200 overflow-hidden">
+              {/* :TABLE HEAD */}
+              <thead className="min-w-full bg-gray-100 text-left text-gray-700">
+                <tr class="border-b border-info-200 bg-info-100 text-neutral-800 ">
+                  {/* ::Name */}
+                  <th
+                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
+                    scope="col"
+                  >
+                    Client-Name
+                  </th>
+                  <th
+                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
+                    scope="col"
+                  >
+                    {" "}
+                    Email
+                  </th>
+                  {/* ::Job Title */}
+                  <th
+                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
+                    scope="col"
+                  >
+                    Message
+                  </th>
+                  {/* ::Email */}
+                  <th
+                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
+                    scope="col"
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
 
-           
-
-        <Footer/>
-
+              {/* :TABLE BODY */}
+              <tbody className="">
+                {msgList.map((val, key) => {
+                  return (
+                    <tr class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                      {/* ::User Name */}
+                      <td className="py-3 px-4 text-base text-gray-700 font-semibold">
+                        {val.name}
+                      </td>
+                      <td className="py-3 px-4 text-base text-gray-700 font-semibold">
+                        {val.Email}
+                      </td>
+                      {/* ::User Job Title */}
+                      <td className="py-3 px-4 text-base text-gray-500 font-medium">
+                        {val.msg}
+                      </td>
+                      {/* ::User Email */}
+                      <td className="py-3 px-4 text-base text-gray-500 font-medium">
+                        <button
+                          className="c-d-btn"
+                          onClick={() => {
+                            deleteMsg(val.id);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
-}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
--- webapp/src/pages/Adminmenu.js
+++ webapp/src/pages/Adminmenu.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 import "../pages/menu.css";
 import {Footer} from "../components/Footer";
+import "./Adminmenu.css";
 
 export const Adminmenu = () =>{
   const [d, setd] = useState([]);
@@ -26,54 +27,49 @@ export const Adminmenu = () =>{
      <div className="nav-none">
        < Navbar/> 
      </div>
-     <div className="consellers">
-     <h2 className="h-mainheading">HISTORY</h2>
-      <div className="c-querie">
+ 
         
-      <div className="mx-auto pb-8 w-full max-w-7xl overflow-x-auto">
-      <table className="px-4 min-w-full rounded-md border border-gray-200 overflow-hidden">
-
-        {/* :TABLE HEAD */}
-        <thead className="min-w-full bg-gray-100 text-left text-gray-700">
-          <tr class="border-b border-info-200 bg-info-100 text-neutral-800 ">
-            {/* ::Name */}
-            <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Client-Name</th>
-            {/* ::Job Title */}
-            <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Date</th>
-            {/* ::Email */}
-            <th className="py-3 px-4 text-sm font-medium uppercase tracking-wide" scope="col">Time</th>
-          </tr>
-        </thead>
+      
 
 
-        {/* :TABLE BODY */}
-        <tbody className="">
         {d.map((val, key) => {
           if(val.conseller===n){
             return (
-              <tr class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-              {/* ::User Name */}
-              <td className="py-3 px-4 text-base text-gray-700 font-semibold">{val.name}</td>
-              {/* ::User Job Title */}
-              <td className="py-3 px-4 text-base text-gray-500 font-medium">{val.date}</td>
-              {/* ::User Email */}
-              <td className="py-3 px-4 text-base text-gray-500 font-medium">{val.time}</td>
-            </tr>
-
+                  <div class="flex  w-full items-center justify-center m-4">
+              <div class="w-full rounded-xl p-12 shadow-2xl shadow-blue-200 md:w-8/12 lg:w-6/12 bg-white">
+                <div class="grid grid-cols-1 gap-6 lg:grid-cols-12">
+                  <div class="grid-cols-1 lg:col-span-3">
+                    <div class="mx-auto flex h-[90px] w-[90px] items-center justify-center rounded-full bg-blue-100 p-4">
+                      <svg id="logo-39" width="50" height="40" viewBox="0 0 50 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M25.0001 0L50 15.0098V24.9863L25.0001 40L0 24.9863V15.0099L25.0001 0Z" fill="#A5B4FC" class="ccompli2"></path>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 15.0098L25 0L50 15.0098V24.9863L25 40L0 24.9863V15.0098ZM25 33.631L44.6967 21.8022V18.1951L44.6957 18.1945L25 30.0197L5.30426 18.1945L5.3033 18.1951V21.8022L25 33.631ZM25 24.5046L40.1018 15.4376L36.4229 13.2298L25 20.0881L13.5771 13.2298L9.89822 15.4376L25 24.5046ZM25 14.573L31.829 10.4729L25 6.37467L18.171 10.4729L25 14.573Z" fill="#4F46E5" class="ccustom"></path>
+                        <path d="M25.0001 0L0 15.0099V24.9863L25 40L25.0001 0Z" fill="#A5B4FC" class="ccompli2" fill-opacity="0.3"></path>
+                      </svg>
+                    </div>
+                  </div>
+            
+                  <div class="col-span-1 lg:col-span-9">
+                    <div class="text-center lg:text-left">
+                      <h2 class="text-2xl font-bold text-zinc-700">{val.name}</h2>
+                      <p class="mt-2 font-semibold text-zinc-700">{val.date}</p>
+                      <p class="mt-4 text-zinc-500">{val.time}</p>
+                    </div>
+            
+                    <div class="mt-6 grid grid-cols-2 gap-4">
+                      <button class="w-full rounded-xl border-2 border-blue-500 bg-white px-3 py-2 font-semibold text-blue-500 hover:bg-blue-500 hover:text-white">Solved</button>
+            
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             );
           }
         })}
-        </tbody>
-
-</table>
-</div>
-
-
-      </div> 
 
 
 
-      </div> 
+
       <Footer/> 
     </div>
     );
