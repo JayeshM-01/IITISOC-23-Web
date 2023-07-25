532ee1727b9d655038a70601a9ace4b36e7a2c93
maripaba2 <jungharerishi@gmail.com>
2023-07-25 14:53:32 +0530

    contact card

--- webapp/src/Register.js
+++ webapp/src/Register.js
@@ -40,7 +40,7 @@ export const Registeration = () => {
   }
 
   return (
-    <div >
+    <div className="loginpage">
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -58,28 +58,45 @@ export const Registeration = () => {
       <div className="nav-none">
         < Navbar />
       </div>
-      <div className="box">
-        <div className="cover">
-          <div className="registration">
-            <h2 className="h2tag">Registration</h2>
-            <label>Username</label>
-            <input
-              type="text"
-              onChange={(e) => {
-                setUsernameReg(e.target.value);
-              }}
-            />
-            <label>Password</label>
-            <input
-              type="text"
-              onChange={(e) => {
-                setPasswordReg(e.target.value);
-              }}
-            />
-            <button className="reglog" onClick={register}> Register </button>
-          </div>
+      <div class=" w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+    <div class="px-6 py-4">
+        <div class="flex justify-center mx-auto">
+            <img class="w-auto h-7 sm:h-8" src="https://th.bing.com/th/id/OIP.LSZDzHyeYI8sgwJpP15o1AHaIZ?w=142&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt=""/>
         </div>
-      </div>
+
+        <h3 class="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">Welcome Back</h3>
+
+        <p class="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
+
+        <form>
+            <div class="w-full mt-4">
+                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" placeholder="Username" aria-label="Username"  onChange={(e) => {
+                setUsernameReg(e.target.value);
+              }}/>
+            </div>
+
+            <div class="w-full mt-4">
+                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" onChange={(e) => {
+                setPasswordReg(e.target.value);
+              }}/>
+            </div>
+
+            <div class="flex items-center justify-between mt-4">
+
+                <button class="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50" onClick={register}>
+                    Sign In
+                </button>
+            </div>
+        </form>
+    </div>
+
+    <div class="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+        <span class="text-sm text-gray-600 dark:text-gray-200">Do you already have an account? </span>
+
+        <a href="/login" class="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Login</a>
+    </div>
+</div>
+      
     </div>);
 }
 
--- webapp/src/pages/Admincontact.js
+++ webapp/src/pages/Admincontact.js
@@ -2,7 +2,7 @@ import { Navbar } from "../Navbar";
 import { Footer } from "../components/Footer";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import "./menu.css";
+import "./Admincontact.css";
 
 export const Admincontact = () => {
   const [msgList, setmsgList] = useState([]);
@@ -24,84 +24,30 @@ export const Admincontact = () => {
   };
 
   return (
-    <div>
+    <div className="hgt">
       <div className="nav">
         <Navbar />
       </div>
-      <div className="consellers">
-        <h2 className="h-mainheading">Queries</h2>
-        <div className="c-querie">
-          <div className="mx-auto pb-8 w-full max-w-7xl overflow-x-auto">
-            <table className="px-4 min-w-full rounded-md border border-gray-200 overflow-hidden">
-              {/* :TABLE HEAD */}
-              <thead className="min-w-full bg-gray-100 text-left text-gray-700">
-                <tr class="border-b border-info-200 bg-info-100 text-neutral-800 ">
-                  {/* ::Name */}
-                  <th
-                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
-                    scope="col"
-                  >
-                    Client-Name
-                  </th>
-                  <th
-                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
-                    scope="col"
-                  >
-                    {" "}
-                    Email
-                  </th>
-                  {/* ::Job Title */}
-                  <th
-                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
-                    scope="col"
-                  >
-                    Message
-                  </th>
-                  {/* ::Email */}
-                  <th
-                    className="py-3 px-4 text-sm font-medium uppercase tracking-wide"
-                    scope="col"
-                  >
-                    Action
-                  </th>
-                </tr>
-              </thead>
-
-              {/* :TABLE BODY */}
-              <tbody className="">
-                {msgList.map((val, key) => {
-                  return (
-                    <tr class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-                      {/* ::User Name */}
-                      <td className="py-3 px-4 text-base text-gray-700 font-semibold">
-                        {val.name}
-                      </td>
-                      <td className="py-3 px-4 text-base text-gray-700 font-semibold">
-                        {val.Email}
-                      </td>
-                      {/* ::User Job Title */}
-                      <td className="py-3 px-4 text-base text-gray-500 font-medium">
-                        {val.msg}
-                      </td>
-                      {/* ::User Email */}
-                      <td className="py-3 px-4 text-base text-gray-500 font-medium">
-                        <button
-                          className="c-d-btn"
-                          onClick={() => {
-                            deleteMsg(val.id);
-                          }}
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                
-                  );
-                })}
-              </tbody>
-            </table>
+    <div className="cardss">
+      {msgList.map((val, key) => {
+        return (
+          <div class="card2">
+            <div class="content">
+              <p class="heading">{val.name} {val.Email}</p>
+              <p class="para">{val.msg}</p>
+              <button
+                class="btn "
+                onClick={() => {
+                  deleteMsg(val.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
+       
+        );
+      })}
       </div>
 
       <Footer />
